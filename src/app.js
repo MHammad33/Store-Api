892645533680a6f41bf2f6cb10afe8ea96acee0d
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-16 11:35:48 +0500

    Add Initial Route for Testing

--- src/app.js
+++ src/app.js
@@ -4,7 +4,13 @@ const app = express();
 const errorHandlerMiddleware = require("./middleware/error-handler.middleware");
 const notFoundMiddleware = require("./middleware/not-found.middleware");
 
+// Middleware
+app.use(express.json());
 
+// Routes
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>products</a>");
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
