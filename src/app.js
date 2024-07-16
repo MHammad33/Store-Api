de016b2fc9fdb509b18f38e36da86311abe1bba8
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-16 11:33:33 +0500

    Add Not Found Middleware

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 
 const errorHandlerMiddleware = require("./middleware/error-handler.middleware");
+const notFoundMiddleware = require("./middleware/not-found.middleware");
 
 
 
-
+app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 module.exports = app;
---
+++ src/middleware/not-found.middleware.js
@@ -0,0 +1,3 @@
+const notFoundMiddleware = (req, res) => res.status(404).json({ msg: "Not found" });
+
+module.exports = notFoundMiddleware;
