6b421774085efc6e31be2674d673aaa57548fa87
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-07-16 11:31:56 +0500

    Add Error Handler Middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
-
 const app = express();
+
+const errorHandlerMiddleware = require("./middleware/error-handler.middleware");
+
+
+
+
+app.use(errorHandlerMiddleware);
 
 module.exports = app;
